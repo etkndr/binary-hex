02b26ae81ccbc0c22882433eddc3e8a314ce6f89
Justin Duncan <justinguyduncan@hotmail.com>
2022-08-18 19:55:09 -0700

    Done

--- conversion-functions-binary-hexa.js
+++ conversion-functions-binary-hexa.js
@@ -7,38 +7,48 @@ Consult documentation on how to utilize the following methods:
 
 /* Base 10 to base 2 */
 function decimalToBinary(blob) {
-  // Your code here
+  const str = blob.toString(2)
+
+  return (`0b${str}`);
 }
 
 /* Base 10 to base 16 */
 function decimalToHexadecimal(blob) {
-  // Your code here
+  const str = blob.toString(16)
+
+  return (`0x${str}`);
 }
 
 /* Base 2 to base 10 */
 function binaryToDecimal(blob) {
-  // Your code here
+  const str = blob.slice(2)
+
+  return parseInt(`${str}`, 2)
+
+
 }
 
 /* Base 16 to base 10 */
 function hexadecimalToDecimal(blob) {
-  // Your code here
+  const str = blob.slice(2)
+
+  return parseInt(`${str}`, 16)
 }
 
-// console.log('Decimal to binary:')
-// console.log(decimalToBinary(13))  // '0b1101'
-// console.log(decimalToBinary(72))  // '0b1001000'
-// console.log('')
+console.log('Decimal to binary:')
+console.log(decimalToBinary(13))  // '0b1101'
+console.log(decimalToBinary(72))  // '0b1001000'
+console.log('')
 
-// console.log('Decimal to hexadecimal:')
-// console.log(decimalToHexadecimal(9))   // '0x9'
-// console.log(decimalToHexadecimal(32))  // '0x20'
-// console.log('')
+console.log('Decimal to hexadecimal:')
+console.log(decimalToHexadecimal(9))   // '0x9'
+console.log(decimalToHexadecimal(32))  // '0x20'
+console.log('')
 
-// console.log('Binary to decimal:')
-// console.log(binaryToDecimal('0b0101'))      // 5
-// console.log(binaryToDecimal('0b10001000'))  // 136
-// console.log('')
+console.log('Binary to decimal:')
+console.log(binaryToDecimal('0b0101'))      // 5
+console.log(binaryToDecimal('0b10001000'))  // 136
+console.log('')
 
 // console.log('Hexadecimal to decimal:')
 // console.log(hexadecimalToDecimal('0x51'))    // 81
